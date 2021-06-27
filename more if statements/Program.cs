c5760353d242d7a6062b8d0b8f3bd39b48eaf67d
jacam666 <ja6cam@gmail.com>
2021-06-27 19:37:01 +0100

    Add project files.

---
+++ more if statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace more_if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there, what is your bill total");
+            int bill = Convert.ToInt32(Console.ReadLine());
+            
+            double discount = (0.15 * bill);
+            if (bill > 1500)
+            {
+                Console.WriteLine("Your total discount is: ");
+                Console.WriteLine(discount);
+            }
+            else
+            {
+                Console.WriteLine("Sorry you have no discount!!!");
+            }
+        }
+    }
+}
